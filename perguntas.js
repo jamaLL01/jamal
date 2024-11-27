7c7f0a53cfec44ab9dd2624d73992a6157819a36
jamaLL01 <00001088378869sp@al.educacao.sp.gov.br>
2024-11-27 18:47:33 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Programação',
+    'O que é uma lista em Python?',
+    'Uma lista em Python é uma estrutura de dados que armazena uma coleção de itens em uma ordem específica.'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual é o maior país do mundo?',
+    'O maior país do mundo em área é a Rússia.'
+)
+
+criaCartao(
+    'Programação',
+    'O que é um algoritmo?',
+    'Um algoritmo é uma sequência de passos definidos para resolver um problema ou realizar uma tarefa.'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz "bom dia" em Inglês?',
+    'Bom dia em inglês é "Good morning" (GUD MÓRNIN).'
+)
